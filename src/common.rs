ba2d8a3aeae28d42dbc0213455ba1e5caed6fc2e
GitHub Action <action@github.com>
2025-04-12 23:11:15 +0000

    Automated changes for RustDesk project

--- src/common.rs
+++ src/common.rs
@@ -944,7 +944,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "https://rd.1128.pp.ua:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
